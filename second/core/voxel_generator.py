5f67825a3ed1e6875b32b7a52093937416c9435e
Leo <zhihao.cui@student.uts.edu.au>
2019-02-09 13:06:20 +1100

    delete print

--- second/core/voxel_generator.py
+++ second/core/voxel_generator.py
@@ -22,7 +22,6 @@ class VoxelGenerator:
         self._grid_size = grid_size
 
     def generate(self, points, max_voxels):
-        print("###### voxel generator runing !!!!")
         return points_to_voxel(
             points, self._voxel_size, self._point_cloud_range,
             self._max_num_points, True, max_voxels)
