39c6e514747d5d51173824cc83d6f322a651d03c
Leo <zhihao.cui@student.uts.edu.au>
2019-02-09 13:15:13 +1100

    create new model path

--- second/train.sh
+++ second/train.sh
@@ -1,1 +1,1 @@
-python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./model
+python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./model_avg_sample
