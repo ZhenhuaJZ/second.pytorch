0072a7c56fc1a6376b22499d2a1e9d0d04bf99ac
LEOCUIZHIHAO <zhihao.cui@student.uts.edu.au>
2019-02-22 16:25:25 +1100

    train.sh path edited

--- second/train.sh
+++ second/train.sh
@@ -1,1 +1,1 @@
-python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./model
+python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./model_eigen_dense_200_2102
