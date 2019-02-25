58d5c9264d389056a49e13f9f9cbcd1c46576d98
Leo <zhihao.cui@student.uts.edu.au>
2019-02-25 18:38:57 +1100

    check if valid point len == num_points_per_voxel

--- second/core/point_cloud/point_cloud_ops.py
+++ second/core/point_cloud/point_cloud_ops.py
@@ -92,8 +92,10 @@ def dense_sampling_v3(voxels, dense_smp_voxels, num_points_per_voxel, voxel_size
 
         # print("[debug-0] num_points_per_voxel[index] : ", num_points_per_voxel[index])
         # print("[debug-1] valid_points_len : ", valid_points_len)
-        if valid_points_len = num_points_per_voxel[index]:
-            print("[debug] valid_points_len = num_points_per_voxel[index]")
+        if valid_points_len == num_points_per_voxel[index]:
+            print("[debug] valid_points_len == num_points_per_voxel[index]")
+        else:
+            print("[debug] not equal")
 
         """error here dont use"""
         #if points in voxels less than 0.2 * max_points then skip the voxels
