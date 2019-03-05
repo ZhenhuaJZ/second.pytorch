3b1e376e5ca26638f7de9c646ecabddbece5a8f3
Leo <zhihao.cui@student.uts.edu.au>
2019-03-05 19:13:29 +1100

    change save path

--- second/train.sh
+++ second/train.sh
@@ -1,1 +1,1 @@
-python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./model_avg_sample_v2
+python ./pytorch/train.py train --config_path=./configs/pointpillars/car/xyres_16.proto --model_dir=./sparse_rpn
